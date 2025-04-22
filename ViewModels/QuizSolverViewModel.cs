f477c03b290d0941fd1d311cf4c4d5b10f91c896
blaze <blazejostaszewski@gmail.com>
2025-04-22 22:55:58 +0200

    zmiany w solver

--- ViewModels/QuizSolverViewModel.cs
+++ ViewModels/QuizSolverViewModel.cs
@@ -1,14 +1,75 @@
 ﻿using Quiz.ViewModels.BaseClass;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Timers;
+using System.Windows;
 
 namespace Quiz.ViewModels
 {
     public class QuizSolverViewModel : ViewModelBase
     {
-        public string Info => "To jest drugi widok.";
+        private string timeLeft = "30,00";
+        public string TimeLeft
+        {
+            get => timeLeft;
+            set
+            {
+                if (timeLeft != value)
+                {
+                    timeLeft = value;
+                    OnPropertyChanged(nameof(TimeLeft));
+                }
+            }
+        }
+
+        private System.Timers.Timer _timer;
+        private double millisecondsLeft;
+
+
+        private ICommand startTimerCommand;
+        public ICommand StartTimerCommand => (startTimerCommand ?? (startTimerCommand = new RelayCommand(
+            p =>
+            {
+                millisecondsLeft = 30_000;
+                TimeLeft = (millisecondsLeft / 1000).ToString("F2");
+
+                _timer = new System.Timers.Timer(10);
+                _timer.Elapsed += (s, e) =>
+                {
+                    millisecondsLeft -= 10;
+
+                    if (millisecondsLeft <= 0)
+                    {
+                        millisecondsLeft = 0;
+                        _timer.Stop();
+
+                        App.Current.Dispatcher.Invoke(() =>
+                        {
+                            TimeLeft = "0,00";
+                            MessageBox.Show(
+                                "Czas dobiegł końca! Sprawdź swój wynik i poprawne odpowiedzi.",
+                                "Koniec!",
+                                MessageBoxButton.OK,
+                                MessageBoxImage.Information
+                            );
+                        });
+
+                        return;
+                    }
+                    App.Current.Dispatcher.Invoke(() =>
+                    {
+                        TimeLeft = (millisecondsLeft / 1000).ToString("F2");
+                    });
+                };
+
+                _timer.Start();
+            },
+            p => true
+        )));
     }
 }
